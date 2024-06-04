e8bfae237cbb63776e6018412f7415048ff88f5e
Vgoutham-2000 <145048734+Vgoutham-2000@users.noreply.github.com>
2024-06-04 18:14:03 +0530

    Update employer.service.ts

--- revhire/src/app/Service/employer.service.ts
+++ revhire/src/app/Service/employer.service.ts
@@ -9,7 +9,7 @@ import { Application } from '../Model/application';
 })
 export class EmployerService {
 
-  base_url = "http://20.75.235.250:8081/";
+  base_url = "http://51.8.10.15:8081/";
   constructor(private http: HttpClient) {   }
 
   public getJob(Id:number):Observable <any>{
