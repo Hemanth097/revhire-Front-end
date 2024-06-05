2ef381b76e1675a2547c9bafc8e63da7df485d5f
Vgoutham-2000 <145048734+Vgoutham-2000@users.noreply.github.com>
2024-06-05 14:01:59 +0530

    Update jobseeker.service.ts

--- revhire/src/app/Service/jobseeker.service.ts
+++ revhire/src/app/Service/jobseeker.service.ts
@@ -8,7 +8,7 @@ import { Application } from '../Model/application';
   providedIn: 'root'
 })
 export class JobseekerService {
-  base_url = "http://51.8.10.15:8081/";
+  base_url = "http://4.255.45.27:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJobsByUserId(id:Number):Observable <any>{
